bd74a35db13bdb51a008ffb76d044f8254249532
Vincent Povirk <vincent@codeweavers.com>
2019-08-14 12:29:59 -0500

    mozfile: Use open() instead of file().
    (cherry picked from commit d5acde9fa9f017244e991d4971fbda08d6b414c3)

--- testing/mozbase/mozfile/mozfile/mozfile.py
+++ testing/mozbase/mozfile/mozfile/mozfile.py
@@ -438,7 +438,7 @@ def load(resource):
 
     if not is_url(resource):
         # if no scheme is given, it is a file path
-        return file(resource)
+        return open(resource)
 
     return urllib.request.urlopen(resource)
 
